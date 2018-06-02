1b2aebdae5c0e9d09fb34401b4cf996be5150dea
abedakhras <abed16021994@gmail.com>
2018-06-03 01:34:35 +0300

    new function was added

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -6,7 +6,15 @@ function sum (a, b) {
   // your code to make the test pass goes here ...
 }
 
+function multi (a,b){
+	return a * b;
+}
+
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+test('multi should return the multiple of two numbers', function (t) {
+  t.equal(3, multi(1, 2)); 
   t.end();
 });
